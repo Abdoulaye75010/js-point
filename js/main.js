880c36319a56f55e28eec601f334565972fa8cca
diagana <ditbakhore@yahoo.fr>
2025-04-10 16:56:16 +0200

    modification2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // Vous mettrez ici le code JavaScript principal
 console.log("point");
 
-
-
 let point = new Point(5, 10)
 
 console.log(point);
@@ -10,3 +8,15 @@ console.log(point);
 console.log(point.abscisse);
 
 console.log(point.ordonnee);
+
+// Clique sur le bouton 
+const bouton = document.getElementById("create_point");
+bouton.addEventListener("click", () => {
+   let abscisse = document.querySelector("#x").value;
+   let ordonnee = document.querySelector("#y").value;
+   console.log(typeof abscisse);
+   abscisse = Number(abscisse);
+   console.log(typeof abscisse);
+
+
+})
